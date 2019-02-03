b7e6ec715459956f6d7e72459fb88cf99b79d9f0
WangPinChieh <pp8101313@hotmail.com>
2019-02-03 21:55:54 +0800

    add Construct Binary Search Tree in PreOrder
    add Construct Binary Search Tree in PreOrder

--- AlgorithmsLearning/BinaryTree.cs
+++ AlgorithmsLearning/BinaryTree.cs
@@ -17,13 +17,13 @@ namespace AlgorithmsLearning
             //tree.left.right = new TreeNode(5);
             //int _Result = FindMinimumDepth(tree);
 
-            TreeNode tree = new TreeNode(5);
-            tree.left = new TreeNode(2);
-            tree.right = new TreeNode(10);
-            tree.left.left = new TreeNode(20);
-            tree.left.right = new TreeNode(1);
-            tree.right.left = new TreeNode(6);
-            tree.right.right = new TreeNode(15);
+            //TreeNode tree = new TreeNode(5);
+            //tree.left = new TreeNode(2);
+            //tree.right = new TreeNode(10);
+            //tree.left.left = new TreeNode(20);
+            //tree.left.right = new TreeNode(1);
+            //tree.right.left = new TreeNode(6);
+            //tree.right.right = new TreeNode(15);
 
             //int _Result = FindMaxSum(tree);
 
@@ -31,10 +31,14 @@ namespace AlgorithmsLearning
             //PrintTreePreOrder(tree);
             //PrintTreePostOrder(tree);
 
-            if (IsFullBinaryTree(tree))
-                Console.Write("true");
-            else
-                Console.Write("false");
+            //if (IsFullBinaryTree(tree))
+            //    Console.Write("true");
+            //else
+            //Console.Write("false");
+            //1 5 7 10 40 50
+            TreeNode[] _TreeNodes = new TreeNode[] { new TreeNode(10), new TreeNode(5), new TreeNode(1), new TreeNode(7), new TreeNode(40), new TreeNode(50) };
+            TreeNode _Root = ConstructBinarySearchTreePreOrder(_TreeNodes);
+
             Console.ReadKey();
         }
         public int FindMinimumDepth(TreeNode node)
@@ -121,16 +125,44 @@ namespace AlgorithmsLearning
             else
                 return false;
         }
-    }
-
-    public class TreeNode
-    {
-        public int data;
-        public TreeNode left, right;
-        public TreeNode(int item)
+        public TreeNode ConstructBinarySearchTreePreOrder(TreeNode[] nodes)
         {
-            data = item;
-            left = right = null;
+            Stack<TreeNode> _Stack = new Stack<TreeNode>();
+            TreeNode _Root = nodes[0];
+            _Stack.Push(_Root);
+
+            for (int i = 1; i < nodes.Length; i++)
+            {
+                TreeNode _Temp = null;
+                while (_Stack.Count > 0 && _Stack.Peek().data < nodes[i].data)
+                {
+                    _Temp = _Stack.Pop();
+                }
+                if (_Temp != null)
+                {
+                    _Temp.right = nodes[i];
+                }
+                else
+                {
+                    _Temp = _Stack.Peek();
+                    _Temp.left = nodes[i];
+                }
+                _Stack.Push(nodes[i]);
+
+            }
+
+            return _Root;
+        }
+
+        public class TreeNode
+        {
+            public int data;
+            public TreeNode left, right;
+            public TreeNode(int item)
+            {
+                data = item;
+                left = right = null;
+            }
         }
     }
 }
